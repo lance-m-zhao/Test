9f83ce43cba076fa809a4d2eb4964056b555013b
Lance <lance.m.zhao@gmail.com>
2017-08-20 15:53:41 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Test.java

--- Test.java
+++ Test.java
@@ -1,12 +1,5 @@
 Class Test {
  public static viod main(String[] args){
-	System.out.println("Test.....");
-	System.out.println("Test....111111111");
-	System.out.println("edit on the github....");
-	System.out.println("edit on Local -----111");
-	System.out.println("edit on Local -----222");
-	System.out.println("edit on Github -----111");
-	System.out.println("edit on Local -----333");
-	System.out.println("edit on Github -----333");
+	System.out.println("edit on Local -----1");
  }
 }
