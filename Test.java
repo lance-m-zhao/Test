07431b150e8ccf71d9c43a0e55d1dc04bd4d6445
Lance <lance.m.zhao@gmail.com>
2017-08-20 16:33:04 +0800

    3

--- Test.java
+++ Test.java
@@ -4,5 +4,7 @@ Class Test {
 	System.out.println("edit on Github -----1");
 	System.out.println("edit on Local -----2");
 	System.out.println("edit on Github -----2");
+	System.out.println("edit on Github -----3");
+	System.out.println("edit on Local -----3");
  }
 }
