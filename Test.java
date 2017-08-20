c4339d292ce7598f9c46db7ce38bb8b30c45a0e0
Lance <lance.m.zhao@gmail.com>
2017-08-20 16:27:46 +0800

    L2

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ Class Test {
  public static viod main(String[] args){
 	System.out.println("edit on Local -----1");
 	System.out.println("edit on Github -----1");
+	System.out.println("edit on Local -----2");
  }
 }
