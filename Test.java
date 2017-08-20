f2843d07725a3a4ee01d5d9631af1b0e5b00f63d
lance-m-zhao <lance.m.zhao@gmail.com>
2017-08-20 16:55:50 +0800

    g8
    g8

--- Test.java
+++ Test.java
@@ -14,5 +14,6 @@ Class Test {
 	System.out.println("edit on Github -----6");
 	System.out.println("edit on Local -----7");
 	System.out.println("edit on Github -----7");
+	System.out.println("edit on Github -----8");
  }
 }
